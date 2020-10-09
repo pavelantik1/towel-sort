6f597c4668b6cd457800050028bfd6c79413e96e
Pavlo Sliusarchuk <sksuzh@gmail.com>
2020-10-09 21:23:52 +0300

    feat: add implementation of sorting

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 
-// You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort (matrix =[]) {
+  function sortArray (argArray = [], isIncreasing = true) {
+    isIncreasing ? argArray.sort( (a,b) => a-b) : argArray.sort( (a,b) => b-a)    
+     return  argArray;
+   }
+
+  let resultArray = [];
+  matrix.forEach( (element, index) => {    
+    Array.isArray(element) ? resultArray.push(...sortArray(element, !(index & 1))) : resultArray.push(element);
+  });
+  return resultArray;
 }
+
+
